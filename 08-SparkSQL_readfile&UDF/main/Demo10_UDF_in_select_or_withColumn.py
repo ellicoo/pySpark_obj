4b6ca0a4041857a0e636d784d08e1f72c8c1fb40
ellicoo <ellicoo@outlook.com>
2024-08-04 19:18:51 +0800

    compare select and withColumn

--- 08-SparkSQL_readfile&UDF/main/Demo10_UDF_in_select_or_withColumn.py
+++ 08-SparkSQL_readfile&UDF/main/Demo10_UDF_in_select_or_withColumn.py
@@ -1,5 +1,5 @@
-from pyspark.sql.functions import F
-
+from pyspark.sql import functions as F
+from pyspark.sql.types import StringType
 
 @F.udf(returnType=StringType())
 def merge_tags(new_df, old_df, fiveTagIDStr):
@@ -61,3 +61,47 @@ def merge_old_df_and_new_df(self, new_df, old_df, fiveTagIDStr):
     )
 
     return result_df
+
+
+# 窗口函数
+    window_spec = Window().partitionBy("LAUNCH_ID")
+
+    # 此处只进行一次join操作，统计量的计算正确
+    # 使用 F.expr 和 IF 进行窗口计算
+    # df_with_counts = df.withColumn(
+    #     "is_browse_cnt",
+    #     F.count(F.expr("IF(is_browse = 1, user_id, NULL)")).over(window_spec)
+    # )
+    result_df = (
+        joined_df.
+            # 假如不存在第一层反欺诈，本统计的是所有参加本次活动的人数
+            withColumn("participants", F.count("*").over(window_spec))
+            # 假如不存在第一层反欺诈，本统计的是（参加本次活动且能抢到货的全体总人数）
+            .withColumn("winner", F.count(F.when(F.col("ENTRY_STATUS") == "WINNER", 1)).over(window_spec))
+            # 1.增加invalid
+            # 假如存在第一层反欺诈，本统计的是：（本层认为是bot的总bot数）--关注投放“机器人数量”
+            .withColumn("invalid", F.count(F.when(F.col("VALIDATION_RESULT") == "INVALID", 1)).over(window_spec))
+            # 假如存在第一层反欺诈，本统计的是：(本层认为是human的总human数)-- 关注“真人数”(非bot)
+            .withColumn("valid", F.count(F.when(F.col("VALIDATION_RESULT") == "VALID", 1)).over(window_spec))
+            .withColumn("ratio_winner_to_valid", F.round(F.col("winner") / F.col("valid"), 3))
+
+    )
+    # 等价操作
+    # result_df = joined_df.select(
+    #     "*",
+    #     F.count(F.expr("if(ENTRY_STATUS = 'WINNER', 1, NULL)")).over(window_spec).alias("winner"),
+    #     F.count(F.expr("if(VALIDATION_RESULT = 'INVALID', 1, NULL)")).over(window_spec).alias("invalid"),
+    #     F.count(F.expr("if(VALIDATION_RESULT = 'VALID', 1, NULL)")).over(window_spec).alias("valid"),
+    #     F.round(F.count(F.expr("if(ENTRY_STATUS = 'WINNER', 1, NULL)")).over(window_spec),
+    #     F.round(F.count(F.expr("if(ENTRY_STATUS = 'WINNER', 1, NULL)")).over(window_spec) /
+    #                 F.count(F.expr("if(VALIDATION_RESULT = 'VALID', 1, NULL)")).over(window_spec), 3).alias("ratio_winner_to_valid")
+
+    # 总结：
+    # 使用 withColumn 和 when 函数确实在处理复杂的转换和中间步骤时更加灵活。它允许你一步步地添加列，并且更容易管理操作之间的依赖关系。
+    # 虽然 select 和 F.expr 也很强大，但在处理表达式和操作顺序时可能需要更加小心
+    # （1）withColumn 和 when：
+    # 优点：逐步添加列，代码清晰，易于理解和维护。每个列的计算是独立的，并且你可以方便地调试每一步。
+    # 缺点：多次调用 withColumn，每次都会生成一个新的 DataFrame 对象，可能会稍微影响性能。
+    # （2）select 和 F.expr：
+    # 优点：可以在一次 select 操作中完成所有的计算。可能在某些情况下，这种方式更高效，因为它避免了多次生成中间 DataFrame。
+    # 缺点：较为复杂，不易于逐步调试和理解每个步骤的计算过程。表达式较长且不易于维护
